95ddc0be713947341c6216c6ba8ed67f96da0865
Mr-robot <hackingtest017@gmail.com>
2023-07-13 14:09:12 -0400

    syntax change

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,13 @@
 -- Stored procedure AddBonus for adding a new correction for students
 DELIMITER $$
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name CHAR(255), IN score INT)
+DROP PROCEDURE IF EXISTS AddBonus;
+CREATE PROCEDURE AddBonus(IN `user_id` INTEGER, IN `project_name` VARCHAR(255), IN `score` INTEGER)
 BEGIN
-	INSERT INTO projects(name)
-	SELECT project_name FROM DUAL
-	WHERE NOT EXISTS(SELECT * FROM projects WHERE name = project_name LIMIT 1)
-	INSERT INTO corrections(user_id, project_id, score)
-	VALUES (user_id, (SELECT  id FROM projects WHERE name = project_name), score);
+    INSERT INTO projects (name)
+    SELECT project_name
+    WHERE project_name NOT IN (SELECT name FROM projects);
+
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
 END $$
 DELIMITER ;$$
