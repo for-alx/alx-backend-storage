f85df6381c59dbdae885b0e43bed8c5e5135e204
Mr-robot <hackingtest017@gmail.com>
2023-07-18 12:51:32 -0400

    doc fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,18 +5,33 @@ Log stats
 from pymongo import MongoClient
 
 
-if __name__ == '__main__':
-    connection = MongoClient('mongodb://localhost:27017')
-    collection = connection.logs.nginx
+def get_log_stats():
+    """
+    get log status
+    Args:
+        //
+    Return:
+        //
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
 
-    print(f'{collection.estimated_document_count()} logs')
 
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    print('Methods:')
-
-    for req in methods:
-        print('\tmethods {}: {}'.format(req,
-              collection.count_documents({'method': req})))
-
-    print('{} status check'.format(collection.count_documents(
-            {'method': 'GET', 'path': '/status'})))
+if __name__ == "__main__":
+    get_log_stats()
