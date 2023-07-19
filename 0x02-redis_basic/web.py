1cc49461dd35905b93764fb3743ca1c59a2ebbac
Mr-robot <hackingtest017@gmail.com>
2023-07-19 16:04:01 -0400

    annotation fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,17 @@
 #!/usr/bin/env python3
 """ web cache module """
+
 import redis
 import requests
-import time
-from functools import wraps
+from datetime import timedelta
 
 
-def cache_with_expiration(time_in_seconds):
+def get_page(url: str) -> str:
     """
     """
-    def decorator(func):
-        """
-        """
-        cache = {}
-
-        @wraps(func)
-        def wrapper(url):
-            """
-            """
-            key = f"count:{url}"
-            if key in cache and time.time() - cache[key][1] < time_in_seconds:
-                return cache[key][0]
-
-            result = func(url)
-            cache[key] = (result, time.time())
-            return result
-
-        return wrapper
-
-    return decorator
-
-
-@cache_with_expiration(10)
-def get_page(url):
-    """
-    """
-    response = requests.get(url)
-    return response.text
+    rredis = redis.Redis()
+    key = "count:{}{}{}".format('{', url, '}')
+    rredis.incr(key)
+    res = requests.get(url)
+    rredis.setex(url, timedelta(seconds=10), res.text)
+    return res.text
