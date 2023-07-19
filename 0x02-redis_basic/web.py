110f843d42b4386e0eb7aedba85b1041ad2edc33
Mr-robot <hackingtest017@gmail.com>
2023-07-19 16:08:39 -0400

    annotation fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
-""" web cache module """
+"""use requests module """
 
 import redis
 import requests
-from datetime import timedelta
+from typing import Callable
+from functools import wraps
 
 
+def access(method: Callable) -> Callable:
+    """decorator for get_page"""
+    @wraps(method)
+    def count(url: str) -> str:
+        """track how many times a particular URL was accessed"""
+        redis_client = redis.Redis()
+        redis_client.incr(f'count:{url}')
+        cached = redis_client.get(f'cached:{url}')
+        if cached:
+            return cached.decode('utf-8')
+        res = method(url)
+        redis_client.setex(f'cached:{url}', 10, res)
+        return res
+    return count
+
+
+@access
 def get_page(url: str) -> str:
-    """
-    """
-    rredis = redis.Redis()
-    key = "count:{}{}{}".format('{', url, '}')
-    rredis.incr(key)
-    res = requests.get(url)
-    rredis.setex(url, timedelta(seconds=10), res.text)
-    return res.text
+    """send request to url"""
+    return requests.get(url).text
 
 
 if __name__ == '__main__':
