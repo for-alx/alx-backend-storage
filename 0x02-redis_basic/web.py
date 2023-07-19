87c6d57ab05a688817aa39ee76e4f015d4383fd6
Mr-robot <hackingtest017@gmail.com>
2023-07-19 16:06:34 -0400

    annotation fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,3 +15,7 @@ def get_page(url: str) -> str:
     res = requests.get(url)
     rredis.setex(url, timedelta(seconds=10), res.text)
     return res.text
+
+
+if __name__ == '__main__':
+    get_page('http://slowwly.robertomurray.co.uk')
